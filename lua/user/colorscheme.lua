06d273becffda8ae1ce02393b2e2a9bd7d31cd8b
Ryan Carl <ryan.carl@viasat.com>
2022-05-02 12:57:35 -0700

    color and cmp setup

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,7 @@
-local colorscheme = "default"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  vim.notify("🤬 colorscheme " .. colorscheme .. " not found!💀")
   return
 end
