22ba84ec873ef8cbb19a8c93ddd9484db10c715c
Ryan Carl <ryan.carl@viasat.com>
2022-05-04 18:00:21 -0700

    format stuff

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -68,6 +68,7 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   -- null LS
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+
   -- Telescope
   use "nvim-telescope/telescope.nvim"
   use 'nvim-telescope/telescope-media-files.nvim'
